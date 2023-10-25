d023fa549cf006d0182f9e7e747fc00f1f94422f
Zasha <0925315@hr.nl>
2023-10-25 15:48:49 +0200

    done

---
+++ src/assets/Vinyl/VinylList.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Flex, Box } from '@react-three/flex'
+import Vinyl from "./Vinyl.jsx";
+import vinyls from '../../data/Vinyl.json'
+import '../../index.css'
+
+export default function VinylList(props) {
+
+    return (
+        <group
+            rotation-y={0.4}
+            position={[-1.75, 2, 0]}
+        >
+            <Flex
+                flexDirection={'row'}
+                justifyContent={'space-between'}
+                flexWrap={'wrap'}
+                size={[8, 0, 0]}
+            >
+                {Object.keys(vinyls).map(key => {
+                    return (
+                        <Box marginRight={0.5} marginBottom={0.5}>
+                            <Vinyl album={key} setSelectedAlbum={props.setSelectedAlbum} />
+                        </Box>
+                    )
+                })}
+            </Flex>
+        </group>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+import {useState} from "react";
+import { Canvas } from '@react-three/fiber'
+import {Scroll, ScrollControls} from '@react-three/drei'
+import VinylList from "./assets/Vinyl/VinylList.jsx"
+import './index.css'
+import VinylInfo from "./assets/Vinyl/VinylInfo.jsx";
+
+function Page() {
+    const [selectedAlbum, setSelectedAlbum] = useState('Lotus')
+
+    return (
+        <div id={'container'}>
+            <div id={'list'}>
+                <Canvas>
+                    <ambientLight />
+                    <pointLight position={[0, 0, 2]} intensity={50} />
+                    <ScrollControls pages={6.5} damping={0.1}>
+                        <Scroll>
+                            <VinylList setSelectedAlbum={setSelectedAlbum} />
+                        </Scroll>
+                    </ScrollControls>
+                </Canvas>
+            </div>
+            <div id={'info'}>
+                <VinylInfo album={selectedAlbum} />
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <Page />
+  </React.StrictMode>
+)
